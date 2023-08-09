f80473e2c8953a5bef6b65e0e7eccdf49c560f4f
burcuustael <burcustael@gmail.com>
2023-08-09 14:15:47 +0300

    Add project files.

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-KOJI8U6;initial catalog=EasyCashDb;integrated security=true");
+
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts {get; set;}
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
